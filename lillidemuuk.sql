80c001407c6ec26d34648a5d9910419c15c32db1
Igor Pimahhov <pimahhov.igor@gmail.com>
2024-04-11 10:13:03 +0300

    tabel kliendikaart on loaded

---
+++ lillidemuuk.sql
@@ -0,0 +1,143 @@
+﻿Create database lillidemyykpimahhov;
+use lillidemyykpimahhov;
+
+create table tootaja(
+tootajaID int primary key identity(1,1),
+eesnimi varchar(20),
+perekonnanimi varchar(20),
+isikukood varchar(11) unique
+)
+select * from tootaja;
+
+insert into tootaja(eesnimi, perekonnanimi,isikukood)
+values ('igor','pimahhov', '506080')
+
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Jdavie', 'Rhucroft', '68180-141');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Marilyn', 'Towlson', '13668-029');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Nerte', 'Stabler', '51830-023');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Ryun', 'Milius', '55330-0001');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Anson', 'Canfer', '0135-0242');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Zea', 'Caunter', '49349-989');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Mollie', 'Roadnight', '59667-0033');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Cher', 'Biagini', '13537-187');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Lanae', 'Markus', '14783-470');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Foster', 'Smails', '49349-549');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Miriam', 'Gaven', '58668-1641');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Nicko', 'Gillice', '17478-605');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Stace', 'Wildey', '21695-096');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Betsey', 'Josskoviz', '59883-826');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Ursuline', 'Aveson', '48951-7042');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Nisse', 'Jonk', '63629-1536');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Timotheus', 'Ashcroft', '11673-658');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Alvin', 'Playhill', '54868-6155');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Kellsie', 'Lomis', '67659-078');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Lauraine', 'Fry', '63629-3563');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Marillin', 'Prozescky', '63548-0064');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Kenn', 'Danilovic', '62499-394');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Kendra', 'Sawdon', '47593-170');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Astra', 'Scriviner', '36987-1794');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Maddi', 'Forbear', '59779-151');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Rochelle', 'Sivorn', '55154-6271');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Katie', 'Jayme', '10019-630');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Johnna', 'Royse', '68479-808');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Piotr', 'Amoss', '14783-201');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Kelcy', 'Delaney', '0517-6110');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Clarinda', 'Albro', '10147-1700');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Lemmy', 'McNair', '15127-994');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Clementius', 'Skacel', '51439-001');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Karlotte', 'Schulter', '68669-711');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Burr', 'St Louis', '11410-923');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Kare', 'Deluca', '53807-152');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Jennilee', 'Robers', '49035-105');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Charlene', 'Lynds', '43353-783');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Rodge', 'Fairfoull', '15127-210');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Gerti', 'Spurett', '52083-232');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Nickolas', 'Dyson', '36987-2637');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Sherye', 'Antowski', '76261-250');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('George', 'Sarjant', '42507-014');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Joy', 'Morfey', '0113-0046');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Ashby', 'Bonny', '0615-7718');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Zola', 'Sell', '64942-1327');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Rhodia', 'Fydo', '51439-001');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Allison', 'Mullin', '53808-0783');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Jaynell', 'Kobpa', '41163-571');
+insert into tootaja (eesnimi, perekonnanimi, isikukood) values ('Hollis', 'Jindracek', '10202-647');
+
+
+--genereeritud andmed on lisatud
+
+-- table kaup
+create table kaup(
+kaupID int primary key identity(1,1),
+kaup varchar(20),
+kirjeldus varchar(100),
+)
+select * from kaup;
+
+insert into kaup(kaup, kirjeldus)
+values ('rimi', 'kartul')
+
+insert into kaup (kaup, kirjeldus) values ('Jeep', 'Liberty');
+insert into kaup (kaup, kirjeldus) values ('Volvo', 'S90');
+insert into kaup (kaup, kirjeldus) values ('Audi', 'A6');
+insert into kaup (kaup, kirjeldus) values ('Chevrolet', 'Equinox');
+insert into kaup (kaup, kirjeldus) values ('Volvo', '940');
+insert into kaup (kaup, kirjeldus) values ('Volvo', 'V40');
+insert into kaup (kaup, kirjeldus) values ('Ford', 'E250');
+insert into kaup (kaup, kirjeldus) values ('Ford', 'F350');
+insert into kaup (kaup, kirjeldus) values ('Dodge', 'Dakota');
+insert into kaup (kaup, kirjeldus) values ('Buick', 'Skylark');
+insert into kaup (kaup, kirjeldus) values ('GMC', 'Savana 2500');
+insert into kaup (kaup, kirjeldus) values ('Suzuki', 'Vitara');
+insert into kaup (kaup, kirjeldus) values ('Dodge', 'Dakota');
+insert into kaup (kaup, kirjeldus) values ('Chevrolet', 'Malibu');
+insert into kaup (kaup, kirjeldus) values ('GMC', 'Yukon');
+insert into kaup (kaup, kirjeldus) values ('Nissan', 'Murano');
+insert into kaup (kaup, kirjeldus) values ('Mitsubishi', 'Diamante');
+insert into kaup (kaup, kirjeldus) values ('Honda', 'Ridgeline');
+insert into kaup (kaup, kirjeldus) values ('Isuzu', 'Rodeo');
+insert into kaup (kaup, kirjeldus) values ('Cadillac', 'Eldorado');
+insert into kaup (kaup, kirjeldus) values ('Porsche', '911');
+insert into kaup (kaup, kirjeldus) values ('Lexus', 'LX');
+insert into kaup (kaup, kirjeldus) values ('Dodge', 'Ram 3500');
+insert into kaup (kaup, kirjeldus) values ('Volkswagen', 'Jetta');
+insert into kaup (kaup, kirjeldus) values ('Chevrolet', 'Express 1500');
+insert into kaup (kaup, kirjeldus) values ('BMW', '645');
+insert into kaup (kaup, kirjeldus) values ('Mazda', 'B-Series');
+insert into kaup (kaup, kirjeldus) values ('Chevrolet', '3500');
+insert into kaup (kaup, kirjeldus) values ('Nissan', 'Frontier');
+insert into kaup (kaup, kirjeldus) values ('Lincoln', 'Navigator');
+insert into kaup (kaup, kirjeldus) values ('Lexus', 'GX');
+insert into kaup (kaup, kirjeldus) values ('Chrysler', 'Cirrus');
+insert into kaup (kaup, kirjeldus) values ('Acura', 'NSX');
+insert into kaup (kaup, kirjeldus) values ('Mazda', 'RX-7');
+insert into kaup (kaup, kirjeldus) values ('Acura', 'TL');
+insert into kaup (kaup, kirjeldus) values ('Toyota', 'MR2');
+insert into kaup (kaup, kirjeldus) values ('Chevrolet', 'Colorado');
+insert into kaup (kaup, kirjeldus) values ('Citroën', 'CX');
+insert into kaup (kaup, kirjeldus) values ('Dodge', 'Ram 3500');
+insert into kaup (kaup, kirjeldus) values ('Pontiac', 'Daewoo Kalos');
+insert into kaup (kaup, kirjeldus) values ('Oldsmobile', '88');
+insert into kaup (kaup, kirjeldus) values ('Honda', 'Pilot');
+insert into kaup (kaup, kirjeldus) values ('Jeep', 'Wrangler');
+insert into kaup (kaup, kirjeldus) values ('Buick', 'Skylark');
+insert into kaup (kaup, kirjeldus) values ('GMC', 'Savana 1500');
+insert into kaup (kaup, kirjeldus) values ('GMC', 'Sierra 1500');
+insert into kaup (kaup, kirjeldus) values ('Honda', 'Civic');
+insert into kaup (kaup, kirjeldus) values ('Nissan', 'Armada');
+insert into kaup (kaup, kirjeldus) values ('Dodge', 'Ram Van B350');
+insert into kaup (kaup, kirjeldus) values ('Lexus', 'LX');
+
+
+
+-- table kliendikaart
+create table  kliendikaart(
+ kliendikaartID int primary key identity(1,1),
+ kliendikaart varchar(20)
+)
+
+select * from  kliendikaart;
+
+insert into kliendikaart(
+kliendikaart)
+values ('platinum'), ('silver'), ('gold');
