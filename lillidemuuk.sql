9acd627b0e0b61cffe1df5e3347e98bb4bb087b7
Igor Pimahhov <pimahhov.igor@gmail.com>
2024-04-11 10:52:47 +0300

    tabel kliendikaart on loaded

--- lillidemuuk.sql
+++ lillidemuuk.sql
@@ -141,3 +141,203 @@ select * from  kliendikaart;
 insert into kliendikaart(
 kliendikaart)
 values ('platinum'), ('silver'), ('gold');
+
+
+
+-- table myyk
+create table myyk(
+ myykID int primary key identity(1,1),
+ kuupaev varchar(20),
+ tootajaID int,
+ kaupID int,
+ kogus int,
+ hind decimal(7,2),
+ kliendikaartID int,
+
+foreign key (tootajaID) references tootaja(tootajaID),
+
+foreign key (kaupID) references kaup(kaupID),
+
+foreign key (kliendikaartID) references kliendikaart(kliendikaartID));
+select * from  myyk;
+
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Ford', 1, 1, '4513', 506, 1);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Nissan', 2, 2, '569', 311, 2);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Volvo', 3, 3, '4500', 132, 3);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Infiniti', 4, 4, '4', 600, 1);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Chevrolet', 5, 5, '778', 855, 2);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Infiniti', 6, 6, '66', 202, 3);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Chevrolet', 7, 7, '1697', 719, 1);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Mazda', 8, 8, '410', 257, 2);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Mercury', 9, 9, '705', 899, 3);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Chevrolet', 10, 10, '0', 985, 1);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('GMC', 11, 11, '100', 475, 2);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Volkswagen', 12, 12, '11', 955, 3);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Kia', 13, 13, '70', 726, 1);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Chrysler', 14, 14, '9', 592, 2);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Chevrolet', 15, 15, '281', 684, 3);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Mazda', 16, 16, '1253', 564, 1);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Mercedes-Benz', 17, 17, '78', 443, 2);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Buick', 18, 18, '279', 398, 3);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Mazda', 19, 19, '1214', 645, 1);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Dodge', 20, 20, '119', 510, 2);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Mercedes-Benz', 21, 21, '12', 488, 3);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Chevrolet', 22, 22, '1110', 586, 1);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Bentley', 23, 23, '115', 350, 2);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Pontiac', 24, 24, '1370', 843, 3);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Lexus', 25, 25, '1487', 194, 1);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Jaguar', 26, 26, '33', 223, 2);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Mitsubishi', 27, 27, '937', 912, 3);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Hyundai', 28, 28, null, 265, 1);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Honda', 29, 29, '16', 825, 2);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Porsche', 30, 30, '3331', 832, 3);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('GMC', 31, 31, '288', 912, 1);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Toyota', 32, 32, '278', 493, 2);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('BMW', 33, 33, '3', 341, 3);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Mercury', 34, 34, '18', 522, 1);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Chevrolet', 35, 35, '9', 492, 2);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Chrysler', 36, 36, null, 317, 3);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Maserati', 37, 37, '250', 310, 1);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Toyota', 38, 38, '302', 872, 2);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Buick', 39, 39, null, 472, 3);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Ford', 40, 40, '33', 550, 1);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Nissan', 41, 41, '75', 616, 2);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Corbin', 42, 42, '35', 343, 3);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Isuzu', 43, 43, null, 374, 1);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Suzuki', 44, 44, '320', 567, 2);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Cadillac', 45, 45, '1923', 687, 3);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Chevrolet', 46, 46, '82', 554, 1);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Chevrolet', 47, 47, '65', 504, 2);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Maybach', 48, 48, '220', 578, 3);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Acura', 49, 49, '1048', 753, 1);
+insert into myyk (kuupaev, tootajaID, kaupID, kogus, hind, kliendikaartID) values ('Audi', 50, 50, '2415', 560, 2);
+
+
+
+create table pusiklientid(
+ pusiklientID int primary key identity(1,1),
+ sagadaminivotab varchar(200),
+ kaupID int,
+ kogus int,
+ kliendikaartID int,
+
+
+ foreign key (kaupID) references kaup(kaupID),
+
+ foreign key (kliendikaartID) references kliendikaart(kliendikaartID));
+
+select * from  pusiklientid;
+
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Chevrolet', 1, '686', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Porsche', 2, '10', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Chevrolet', 3, '3331', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Buick', 4, '2165', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Nissan', 5, '0', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Dodge', 6, '15', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Volkswagen', 7, null, 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Ford', 8, '3', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Lincoln', 9, '1365', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Oldsmobile', 10, '226', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Chevrolet', 11, '374', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('GMC', 12, '192', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Lincoln', 13, '1305', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Volvo', 14, '22', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('GMC', 15, '0', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Subaru', 16, '1720', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Cadillac', 17, '4006', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Dodge', 18, '3877', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Dodge', 19, '107', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Honda', 20, '2743', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Dodge', 21, '127', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Volkswagen', 22, '10', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Chrysler', 23, '31', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Acura', 24, '36', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Audi', 25, '650', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Land Rover', 26, '52', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Ford', 27, '1182', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Acura', 28, '3970', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Dodge', 29, '538', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Ford', 30, '25', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Infiniti', 31, '3904', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Ford', 32, '360', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Nissan', 33, '311', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Ford', 34, '984', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Honda', 35, '174', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Chevrolet', 36, '5', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Lamborghini', 37, '305', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Cadillac', 38, '98', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Mercury', 39, '46', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Volvo', 40, '53', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Subaru', 41, '25', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Mitsubishi', 42, '35', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Saab', 43, '955', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Acura', 44, '3300', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Chevrolet', 45, '847', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Audi', 46, '13', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Lincoln', 47, '294', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Nissan', 48, '2061', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Ford', 49, '2848', 3);
+insert into pusiklientid (sagadaminivotab, kaupID, kogus, kliendikaartID) values ('Kia', 50, '1847', 3);
+
+
+create table enammuudmark(
+ enammuudmarkID int primary key identity(1,1),
+ mark varchar(200),
+ kogus int,
+ pusiklientID int,
+
+
+ foreign key (pusiklientID) references pusiklientid(pusiklientID));
+
+select * from  enammuudmark;
+
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Dodge', '886', 1);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Buick', '284', 2);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Jeep', '1208', 3);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('BMW', '58', 4);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Hummer', '28', 5);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Chrysler', '3', 6);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Infiniti', '1048', 7);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Cadillac', '33', 8);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Porsche', '56', 9);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('GMC', '1330', 10);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Mitsubishi', '105', 11);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Ford', '768', 12);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Mercedes-Benz', '6500', 13);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Dodge', '1700', 14);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Eagle', '935', 15);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Porsche', '2925', 16);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('GMC', '1267', 17);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Mercury', '15', 18);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('BMW', '2613', 19);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Buick', '1217', 20);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Mercedes-Benz', '984', 21);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Lamborghini', '412', 22);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Infiniti', '157', 23);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Chevrolet', '1198', 24);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Bentley', '2690', 25);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Toyota', '12', 26);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Mitsubishi', '1464', 27);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Acura', '2106', 28);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Ford', '1447', 29);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Pontiac', '287', 30);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('GMC', null, 31);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Pontiac', '55', 32);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Audi', '9', 33);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Chevrolet', '3400', 34);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Chevrolet', '1017', 35);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('BMW', '1881', 36);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Mazda', '22', 37);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Toyota', '768', 38);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Audi', '16', 39);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Toyota', '2018', 40);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('BMW', '642', 41);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Nissan', '7', 42);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Pontiac', '1465', 43);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Subaru', '3510', 44);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Infiniti', '587', 45);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Mercury', '478', 46);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Ford', '106', 47);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Mitsubishi', null, 48);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('BMW', null, 49);
+insert into enammuudmark (mark, kogus, pusiklientID) values ('Volkswagen', null, 50);
